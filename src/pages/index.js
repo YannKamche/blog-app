351765fd8bbff4f268d6feeaa36bb53ed161210e
Yann Kamche <yannkamche@gmail.com>
2024-03-30 03:10:44 +0100

    Completed basic structure

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from './Home'
+import About from './About'
+import AddEditBlog from './AddEditBlog'
+import Auth from './Auth'
+import Details from './Details'
+import NotFound from './NotFound'
+
+export {
+    Home,
+    About,
+    AddEditBlog,
+    Auth,
+    Details,
+    NotFound
+}
