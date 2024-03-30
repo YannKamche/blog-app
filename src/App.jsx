4d4879076ca26e3167ac014b054d85821e10724d
Yann Kamche <yannkamche@gmail.com>
2024-03-30 03:22:54 +0100

    Route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Home } from './pages/index'
+import { About, AddEditBlog, Details, Home, NotFound } from './pages/index'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/detail/:id' element={<Details />} />
+        <Route path='/create' element={<AddEditBlog />} />
+        <Route path='/update/:id' element={<AddEditBlog />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
