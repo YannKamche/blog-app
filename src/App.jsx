7b484e5ae55dfb49da4fdaad05be4bc1f5271732
Yann Kamche <yannkamche@gmail.com>
2024-03-30 03:13:48 +0100

    React Toastify configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Home } from './pages/index'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 const App = () => {
   return (
     <div>
